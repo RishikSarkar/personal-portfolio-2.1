f8896059e0e7b586c429c9c2b49df1a80f135fb2
Rishik Sarkar <rs1894@scarletmail.rutgers.edu>
2023-04-05 18:47:09 -0400

    pdf size changes

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -17,7 +17,7 @@ function Resume() {
   }
 
   return (
-    <div className='font-montserrat w-screen selection:text-[#012033] selection:bg-[#40e0d0] dark:selection:text-[#ecf0f3] dark:selection:bg-[#008080] h-screen text-center bg-[#012033] dark:bg-[#ecf0f3]'>
+    <div className='font-montserrat md:w-screen selection:text-[#012033] selection:bg-[#40e0d0] dark:selection:text-[#ecf0f3] dark:selection:bg-[#008080] h-screen text-center bg-[#012033] dark:bg-[#ecf0f3]'>
       <div className='h-[85%] mx-auto p-20 flex justify-center items-top'>
         <div>
           <p className='py-5 text-2xl uppercase tracking-widest text-[#ecf0f3] dark:text-[#012033]'>
@@ -25,10 +25,10 @@ function Resume() {
           </p>
           <div className='pb-8'>
             <Document file='../assets/files/Resume.pdf' onLoadSuccess={onDocumentLoadSuccess}>
-              <Page pageNumber={pageNumber} renderTextLayer={false} />
+              <Page size="A4" pageNumber={pageNumber} renderTextLayer={false} />
             </Document>
           </div>
-          <div className='items-center mx-auto -mt-12 md:mt-0 md:pb-8 grid'>
+          <div className='items-center mx-auto md:pb-8 grid'>
             <Link href='/#about'>
               <p className='uppercase underline cursor-pointer text-[#ecf0f3] dark:text-[#012033] hover:text-[#BBEBE9] dark:hover:text-[#008080]'>Back</p>
             </Link>
